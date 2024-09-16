6fb2145e21211985cd09431d531705a60731e15c
rafia9005 <rafia9005@gmail.com>
2024-09-16 11:58:27 +0700

    generate routes and server

---
+++ cmd/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"EduPay/routes"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	app := fiber.New(fiber.Config{
+		AppName:      "Tuxedo BackEnd",
+		ServerHeader: "Tuxedo",
+		BodyLimit:    10 * 1024 * 1024,
+	})
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	// database.Connect()
+
+	// routes.AutoMigrate()
+
+	// app.Use(cors.New(cors.Config{
+	// 	AllowOrigins:     "http://localhost:5173, https://tuxedo-frontend.vercel.app",
+	// 	AllowMethods:     "GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD",
+	// 	AllowHeaders:     "Origin, Content-Type, Accept, Authorization, x-token",
+	// 	ExposeHeaders:    "Content-Length",
+	// 	AllowCredentials: true,
+	// }))
+
+	app.Static("/", "./public")
+	routes.SetupRouter(app)
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := app.Listen(":" + port); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
+}
---
+++ routes/routes.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRouter(r *fiber.App) {
+	// app := r.Group("/api")
+	// authentication
+}
